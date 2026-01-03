d0248e7b53b97be5a50c143ec956950d5c097bf9
pritisharma1511 <ps0130815@gmail.com>
2026-01-03 17:20:50 +0530

    fronted routes added

--- ai-learning-assistant/src/App.jsx
+++ ai-learning-assistant/src/App.jsx
@@ -3,6 +3,16 @@ import { BrowserRouter as Router,Routes,Route,Navigate } from 'react-router-dom'
 import LoginPage from './pages/Auth/LoginPage';
 import RegisterPage from './pages/Auth/RegisterPage';
 import NotFoundPage from './pages/Quizzes/NotFoundPage';  
+import ProtectedRoute from './components/auth/ProtectedRoute';
+import DashboardPage from './pages/Dashboard/DashboardPage';
+import DocumentListPage from './pages/Documents/DocumentListPage';
+import DocumentDetailPage from './pages/Documents/DocumentDetailPage';
+import FlashcardListPage from './pages/Flashcards/FlashcardListPage';
+import FlashcardPage from './pages/Flashcards/FlashcardPage';
+import QuizTakePage from './pages/Quizzes/QuizTakePage';
+import QuizResultPage from './pages/Quizzes/QuizResultPage';
+import ProfilePage from './pages/Profile/ProfilePage';
+
 
 const App = () => {
   const isAuthenticated = true; // Replace with actual authentication logic
@@ -27,8 +37,15 @@ const App = () => {
         <Route path ="/register" element={<RegisterPage />} />
 
          <Route element={<ProtectedRoute />}>
-           <Route path="/dashboard" element={<div>Dashboard Page</div>} />
-           <Route path="/docum" element={<div>Profile Page</div>} />
+           <Route path="/dashboard" element={<DashboardPage />} />
+           <Route path="/documents" element={<DocumentListPage />} />
+            <Route path="/documents/:id" element={<DocumentDetailPage />} />
+            <Route path="/flashcards" element={<FlashcardListPage />} />
+            <Route path="/documents/:id/flashcards" element={<FlashcardPage />} />
+            <Route path="/quizzes/:quizId" element={<QuizTakePage />} />
+            <Route path="/quizzes/:quizId/result" element={<QuizResultPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+         </Route>
 
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
